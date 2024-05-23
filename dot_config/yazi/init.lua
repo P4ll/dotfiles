7f20d2c8d6b293dfc6c9f4e7c28bdd228bc36946
Андрей Танькин <cornelius.weling@gmail.com>
2024-05-23 22:52:10 +0400

    chezmoi: backup at 2024-05-23 18:52:08.588 UTC

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,13 @@
+require("bookmarks"):setup({
+	save_last_directory = false,
+	persist = "vim",
+	notify = {
+		enable = false,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
